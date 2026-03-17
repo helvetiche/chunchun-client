7c4291b7272edc0bec6ff993dec70b332e32697f
Nasche <helvetiche@gmail.com>
2026-03-17 10:57:20 +0800

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,11 +4,9 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-
 export const unstable_settings = {
   anchor: '(tabs)',
 };
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
